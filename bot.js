7f9d947ea176ff94ebb14daae167fd916496d35c
Mustafagaming <36731461+Mustafagaming@users.noreply.github.com>
2018-08-05 14:19:45 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -203,6 +203,5 @@ client.on('guildMemberRemove', member => {
            
        .setDescription(" مدة الرابط : ساعه  عدد استخدامات الرابط : 1 ")
          message.author.sendEmbed(Embed11)
-       });
-      
+      })
 client.login(process.env.BOT_TOKEN);
