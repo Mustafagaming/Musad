5b502351a307709a9a800da2aebb4ebc151bc61e
Mustafagaming <36731461+Mustafagaming@users.noreply.github.com>
2018-08-05 13:29:44 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -379,4 +379,206 @@ client.on('message', message => {
      }
    });
 
+client.on('message', message => {
+    if (message.content.startsWith(prefix + "deafen")) {
+      if (!message.member.hasPermission('DEAFEN_MEMBERS')) return;
+    { message.member.setDeaf(true);
+      }
+    }
+      });
+  client.on('message', message => {
+    if (message.content.startsWith(prefix + "vmute")) {
+      if (!message.member.hasPermission('MUTE_MEMBERS')) return;
+    { message.member.setMute(true);
+      }
+    }
+      });
+  client.on('message', message => {
+    if (message.content.startsWith(prefix + "undeafen")) {
+      if (!message.member.hasPermission('DEAFEN_MEMBERS')) return;
+    { message.member.setDeaf(false);
+      }
+    }
+      });
+  client.on('message', message => {
+    if (message.content.startsWith(prefix + "vunmute")) {
+      if (!message.member.hasPermission('MUTE_MEMBERS')) return;
+    { message.member.setMute(false);
+      }
+    }
+      });
+
+      client.on('message', message => {
+        if (message.guild) {
+       let embed = new Discord.RichEmbed()
+        let args = message.content.split(' ').slice(1).join(' ');
+      if(message.content.split(' ')[0] == prefix + 'bc') {
+        if (!args[1]) {
+      message.channel.send("**^bc <message>**");
+      return;
+      }
+            message.guild.members.forEach(m => {
+       if(!message.member.hasPermission('ADMINISTRATOR')) return;
+                var bc = new Discord.RichEmbed()
+                .setAuthor(message.author.username, message.author.avatarURL)
+                .addField(' The server', `${message.guild.name}`, true)
+                .addField(' who sended the messege ', `${message.author.username}!${message.author.discriminator}`, true)
+                .addField(' the messege ', args)
+                .setThumbnail(message.guild.iconURL)
+                .setColor('RANDOM')
+                m.send(`${m}`,{embed: bc});
+            });
+            const unknown = new Discord.RichEmbed()
+            .setAuthor(message.author.username, message.author.avatarURL)
+            .setTitle('✅| the messege is loading ')
+            .addBlankField(true)
+            .addField('♨| i got sended to  ', message.guild.memberCount , true)
+            .addField('📝| the message ', args)
+            .setColor('RANDOM')
+            message.channel.sendEmbed(embed);
+        }
+        } else {
+            return;
+        }
+      });
+
+      client.on('message', message => {
+        var prefix = "M.";
+               if(message.content === prefix + "mutechannel") {
+                                   if(!message.channel.guild) return message.reply('** This command only for servers**');
+        
+           if(!message.member.hasPermission('MANAGE_MESSAGES')) return message.reply(' **__ليس لديك صلاحيات__**');
+                      message.channel.overwritePermissions(message.guild.id, {
+                    SEND_MESSAGES: false
+        
+                      }).then(() => {
+                          message.reply("**__تم تقفيل الشات__ ✅ **")
+                      });
+                        }
+        //FIRE BOT
+            if(message.content === prefix + "unmutechannel") {
+                                if(!message.channel.guild) return message.reply('** This command only for servers**');
+        
+           if(!message.member.hasPermission('MANAGE_MESSAGES')) return message.reply('**__ليس لديك صلاحيات__**');
+                      message.channel.overwritePermissions(message.guild.id, {
+                    SEND_MESSAGES: true
+        
+                      }).then(() => {
+                          message.reply("**__تم فتح الشات__✅**")
+                      });
+            }
+               
+        });
+
+        client.on("message", message => {
+            var prefix = "M."
+            if (!message.content.startsWith(prefix)) return;
+              let command = message.content.split(" ")[0];
+              command = command.slice(prefix.length);
+                if(command === "skin") {
+                        const args = message.content.split(" ").slice(1).join(" ")
+                if (!args) return message.channel.send("** Type your skin name **");
+                const image = new Discord.Attachment(`https://visage.surgeplay.com/full/256/${args}`, "skin.png");
+            message.channel.send(image)
+                }
+            });
+
+            client.on('guildCreate', guild => {
+                var embed = new Discord.RichEmbed()
+                .setColor(0x5500ff)
+                .setDescription(`**شكراً لك لإضافه البوت الى سيرفرك**`)
+                    guild.owner.send(embed)
+              });
+        
+              client.on("message", message => { //clear
+                var args = message.content.substring(prefix.length).split(" ");
+                if (message.content.startsWith(prefix + "clear")) {
+                    if(!message.channel.guild) return message.reply('**❌ اسف لكن هذا الامر للسيرفرات فقط **');         
+       if(!message.member.hasPermission('MANAGE_MESSAGES')) return message.reply('**⚠  لا يوجد لديك صلاحية لمسح الشات**');
+            var msg;
+            msg = parseInt();
+          
+          message.channel.fetchMessages({limit: msg}).then(messages => message.channel.bulkDelete(messages)).catch(console.error);
+          message.channel.sendMessage("", {embed: {
+            title: "``تــم مسح الشات ``",
+            color: 0x5016f3, 
+            footer: {
+              
+            }
+          }}).then(msg => {msg.delete(3000)});
+                              }
+    
+         
+    });
+
+
+    client.on('message', message => {
+        if (message.author.bot) return;
+       if (message.content.startsWith("رابط")) {
+           message.channel.createInvite({
+           thing: true,
+           maxUses: 1,
+           maxAge: 3600,
+       }).then(invite =>
+         message.author.sendMessage(invite.url)
+       )
+       const embed = new Discord.RichEmbed()
+           .setColor("RANDOM")
+             .setDescription(" تم أرسال الرابط برسالة خاصة ")
+              .setAuthor(client.user.username, client.user.avatarURL)
+                    .setAuthor(client.user.username, client.user.avatarURL)
+                   .setFooter('طلب بواسطة: ' + message.author.tag)
+   
+         message.channel.sendEmbed(embed).then(message => {message.delete(10000)})
+                 const Embed11 = new Discord.RichEmbed()
+           .setColor("RANDOM")
+           
+       .setDescription(" مدة الرابط : ساعه  عدد استخدامات الرابط : 1 ")
+         message.author.sendEmbed(Embed11)
+    }
+
+       client.on('message', message => {
+        var prefix = "M.";
+              if(message.content === prefix + "hchannel") {
+              if(!message.channel.guild) return;
+              if(!message.member.hasPermission('ADMINISTRATOR')) return message.reply('You Dont Have Perms ❌');
+                     message.channel.overwritePermissions(message.guild.id, {
+                     READ_MESSAGES: false
+         })
+                      message.channel.send('Channel Hided Successfully ! ✅  ')
+         }
+        });
+
+
+        client.on('message', message =>{
+            let messageArray = message.content.split(" ");
+            let cmd = messageArray[0];
+            let args = messageArray.slice(1);
+            let prefix = 'M.';
+             
+            if(cmd === `${prefix}report`){
+                let rUser = message.guild.member(message.mentions.users.first() || message.guild.members.get(args[0]));
+                if(!rUser) return message.channel.send("Idk who 2 report ??");
+                let reason = args.join(" ").slice(22);
+                if(!reason) return message.channel.send("What is the reason ??");
+            
+                let reportEmbed = new Discord.RichEmbed()
+                .setTitle("User just reported...")
+                .setColor("#f7abab")
+                .addField("- Reported User :", `${rUser} (${rUser.id})`)
+                .addField("- Reported By :", `${message.author} (${message.author.id})`)
+                .addField("- Reported In :", message.channel)
+                .addField("- Report Time :", message.createdAt.toLocaleString(),true)
+                .addField("- Reason :", reason);
+            
+                let reportschannel = message.guild.channels.find(`name`, "reports");
+                if(!reportschannel) return message.channel.send("You should to make `reports` channel.");
+            
+            
+                message.delete().catch(O_o=>{});
+                message.author.send(`<@${rUser.id}>, Reported Successfully!!`)
+                reportschannel.send(reportEmbed);
+            };
+        });
+
 client.login(process.env.BOT_TOKEN);
